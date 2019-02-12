0aa9484a96d74c62fbff14ff06bd8077c1c27c8a
Christian  Torres <chris@Christians-MacBook-Pro.local>
2019-02-12 15:29:50 -0500

    Add login view with HOOKS <3

--- web/src/views/Login/Form/index.jsx
+++ web/src/views/Login/Form/index.jsx
@@ -1,5 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+function userFormInput(initialValue) {
+  const [value, setValue] = useState(initialValue)
+
+  function handleChange(e) {
+    setValue(e.target.value)
+  }
+  return {
+    value,
+    onChange: handleChange
+  }
+}
 
 export default function LoginForm() {
-  return <div>Welcome login</div>
+  const username = userFormInput('')
+  const password = userFormInput('')
+  return (
+    <React.Fragment>
+      <form>
+        <span>User: </span>
+        <input {...username} />
+        <br />
+        <span>Password: </span>
+        <input {...password} />
+        <br />
+        <button type="submit" disabled={!username || !password}>
+          Login
+        </button>
+      </form>
+    </React.Fragment>
+  )
 }
