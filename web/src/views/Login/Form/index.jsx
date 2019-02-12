71d787b6856fa09a9724e02d13d9e621c3fc978c
Christian  Torres <chris@Christians-MacBook-Pro.local>
2019-02-12 15:52:05 -0500

    End login verification

--- web/src/views/Login/Form/index.jsx
+++ web/src/views/Login/Form/index.jsx
@@ -1,4 +1,15 @@
 import React, { useState } from 'react'
+import gql from 'graphql-tag'
+import queryString from 'query-string'
+import { Mutation } from 'react-apollo'
+
+const LOGIN_USER_MUTATION = gql`
+  mutation loginUserMutation($input: LoginUserInput!) {
+    loginUser(input: $input) {
+      token
+    }
+  }
+`
 
 function userFormInput(initialValue) {
   const [value, setValue] = useState(initialValue)
@@ -12,22 +23,55 @@ function userFormInput(initialValue) {
   }
 }
 
-export default function LoginForm() {
+export default function LoginForm({ history, location }) {
   const username = userFormInput('')
   const password = userFormInput('')
   return (
-    <React.Fragment>
-      <form>
-        <span>User: </span>
-        <input {...username} />
-        <br />
-        <span>Password: </span>
-        <input {...password} />
-        <br />
-        <button type="submit" disabled={!username || !password}>
-          Login
-        </button>
-      </form>
-    </React.Fragment>
+    <Mutation mutation={LOGIN_USER_MUTATION}>
+      {(loginUser, { loading }) => (
+        <form
+          onSubmit={async e => {
+            e.preventDefault()
+            try {
+              if (!username.value || !password.value) {
+                return
+              }
+
+              const response = await loginUser({
+                variables: {
+                  input: { username: username.value, password: password.value }
+                }
+              })
+
+              if (response) {
+                const { token } = response.data.loginUser
+                window.sessionStorage.setItem('token', token)
+
+                const { r } = queryString.parse(location.search)
+
+                if (r && typeof r === 'string') {
+                  history.replace(r)
+                } else {
+                  history.replace('/')
+                }
+              }
+            } catch (error) {
+              // eslint-disable-next-line no-console
+              console.log('something is wrong', error)
+            }
+          }}
+        >
+          <span>User: </span>
+          <input {...username} />
+          <br />
+          <span>Password: </span>
+          <input type="password" {...password} />
+          <br />
+          <button type="submit" disabled={!username.value || !password.value}>
+            {loading ? 'Loading...' : 'Login'}
+          </button>
+        </form>
+      )}
+    </Mutation>
   )
 }
