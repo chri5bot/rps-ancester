b48346bd8332382f9dbfaa685b487659bbea6301
Christian  Torres <chris@Christians-MacBook-Pro.local>
2019-02-11 23:41:13 -0500

    Add routes and views

--- web/src/App.js
+++ web/src/App.js
@@ -4,22 +4,16 @@ import {ThemeProvider} from 'styled-components'
 import {createClient} from './client'
 import {GlobalStyle} from './styles/reset.css'
 import theme from './styles/theme'
-
+import Routes from './routes'
 
 export default function App(){
   return (
     <ApolloProvider client={createClient()}>
       <ThemeProvider theme={theme}>
-      <React.Fragment>
-      
-        <GlobalStyle>
-          
-        </GlobalStyle>
-       
-            hey
-       
-
-      </React.Fragment>
+        <React.Fragment>
+          <GlobalStyle/>
+          <Routes/>
+        </React.Fragment>
       </ThemeProvider>
     </ApolloProvider>
   )
---
+++ web/src/routes/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Route, Switch } from 'react-router'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+import Home from '../views/Home'
+import LoginForm from '../views/Login/Form'
+
+export default function Routes(){
+    return(
+        <Router>
+            <Switch>
+                <Route path="/home" component={Home}/>
+                <Route path="/login" component={LoginForm}/>
+            </Switch>
+        </Router>
+    )
+}
