2bc78614a254fa997fe73655a689cc3a3f839863
Christian  Torres <chris@Christians-MacBook-Pro.local>
2019-02-12 10:41:58 -0500

    Add logic of login and redirect if isnt

--- web/src/client.js
+++ web/src/client.js
@@ -1,11 +1,46 @@
 import { ApolloClient } from 'apollo-client'
-import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { onError } from 'apollo-link-error'
+import { createUploadLink } from 'apollo-upload-client'
+import { setContext } from 'apollo-link-context'
+import { API_URL } from './config'
 
 // eslint-disable-next-line import/prefer-default-export
 export const createClient = () => {
+  const errorsLink = onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors) {
+      graphQLErrors.map(({ message, locations, path }) =>
+        // eslint-disable-next-line no-console
+        console.log(
+          `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+        )
+      )
+    }
+
+    if (networkError) {
+      // eslint-disable-next-line no-console
+      console.log(`[Network error]: ${networkError}`)
+    }
+  })
+
+  const link = errorsLink.concat(
+    createUploadLink({
+      uri: API_URL
+    })
+  )
+
+  const authLink = setContext((_, { headers }) => {
+    const token = window.sessionStorage.getItem('token')
+    return {
+      headers: {
+        ...headers,
+        authorization: token || ''
+      }
+    }
+  })
+
   return new ApolloClient({
-    link: new HttpLink(),
+    link: authLink.concat(link),
     cache: new InMemoryCache()
   })
 }
--- web/src/routes/index.jsx
+++ web/src/routes/index.jsx
@@ -1,16 +1,21 @@
 import React from 'react'
 import { Route, Switch } from 'react-router'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Redirect } from 'react-router-dom'
+
+import signedOutFallback from '../helpers/signed-out-fallback'
 
 import Home from '../views/Home'
 import LoginForm from '../views/Login/Form'
+
+const LoginFallBack = signedOutFallback(() => <Redirect to="/" />, LoginForm)
+const HomeFallBack = signedOutFallback(Home, () => <Redirect to="/login" />)
 
 export default function Routes() {
   return (
     <Router>
       <Switch>
-        <Route path="/home" component={Home} />
-        <Route path="/login" component={LoginForm} />
+        <Route path="/" exact component={HomeFallBack} />
+        <Route path="/login" exact component={LoginFallBack} />
       </Switch>
     </Router>
   )
